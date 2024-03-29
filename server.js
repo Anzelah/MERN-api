0e3f7f3d0a920b041fd4d187ab9cd9c755637db5
Anzelah <jepkoechanzellah@gmail.com>
2024-03-29 19:27:48 +0300

    connect to mongoDb database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 const port = 3000
@@ -6,6 +7,11 @@ const port = 3000
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
-app.listen(port, () => {
+
+mongoose.connect('mongodb+srv://admin:Angelica87@subscriberapi.rlg8ubn.mongodb.net/Node-API?retryWrites=true&w=majority&appName=SubscriberAPI')
+.then(() => {
+    console.log('Connected to MongoDB Database...')
+    app.listen(port, () => {
     console.log(`Node API application listening on port: ${port}`)
-})
+    })
+}).catch((err) => console.log(err))
