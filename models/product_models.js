812c6aede0a94f251f1922519bcc05c6a7c944d5
Anzelah <jepkoechanzellah@gmail.com>
2024-03-29 19:45:28 +0300

    create a products schema

---
+++ models/product_models.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const productsSchema = new Schema(
+    {
+        name: { type: String, required: [true, "Please input a product name"]},
+        quantity: { type: Number, required: true, default: 0 },
+        price: { type: Number, required: true },
+        image: { type: String, required: false }
+    },
+    {
+        timestamps: true
+    }
+)
