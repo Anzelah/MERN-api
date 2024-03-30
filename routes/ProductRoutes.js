69e9c4a67fb33e8e634992cfd40f7b0448451697
Anzelah <jepkoechanzellah@gmail.com>
2024-03-30 15:49:49 +0300

    refractor code and place all routes in the route folder

---
+++ routes/ProductRoutes.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const Product = require('../models/product_models')
+
+const router = express.Router()
+
+
+// GET method
+router.get('/products', async(req, res) => {
+    try {
+        const product = await Product.find({})
+        res.status(200).json(product)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+})
+
+router.get('/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// POST method
+router.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(201).json(req.body)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// PUT method
+router.put('/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+        
+        if (!product) return res.status(404).json({ message: `Cannot find product with ID in database ${id}`})
+        
+        const updatedProduct = await Product.findById(id)
+        res.status(200).json(updatedProduct)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// DELETE method
+router.delete('/products/:id', async(req, res) => {
+    try {
+        const { id } = req.params
+        const product = await Product.findByIdAndDelete(id)
+        
+        if (!product) return res.status(404).json({ message: `Cannot find product with ID ${id}`})
+        
+        res.status(200).json(product)
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ message: error.message })
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/product_models')
+const productRoute = require('./routes/ProductRoutes')
 const app = express()
 
 
@@ -9,74 +9,11 @@ const MONGO_URI = process.env.MONGO_URI
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+// routes
+app.use('/api', productRoute)
 
-
-// GET method
 app.get('/', (req, res) => {
     res.send('Hello world!')
-})
-
-app.get('/products', async(req, res) => {
-    try {
-        const product = await Product.find({})
-        res.status(200).json(product)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: error.message })
-    }
-})
-
-app.get('/products/:id', async(req, res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findById(id)
-        res.status(200).json(product)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ message: error.message })
-    }
-})
-
-// POST method
-app.post('/products', async(req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(201).json(req.body)
-    } catch(error) {
-        console.log(error)
-        res.status(500).json({ message: error.message })
-    }
-})
-
-// PUT method
-app.put('/products/:id', async(req, res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findByIdAndUpdate(id, req.body)
-        
-        if (!product) return res.status(404).json({ message: `Cannot find product with ID in database ${id}`})
-        
-        const updatedProduct = await Product.findById(id)
-        res.status(200).json(updatedProduct)
-    } catch(error) {
-        console.log(error)
-        res.status(500).json({ message: error.message })
-    }
-})
-
-// DELETE method
-app.delete('/products/:id', async(req, res) => {
-    try {
-        const { id } = req.params
-        const product = await Product.findByIdAndDelete(id)
-        
-        if (!product) return res.status(404).json({ message: `Cannot find product with ID ${id}`})
-        
-        res.status(200).json(product)
-    } catch(error) {
-        console.log(error)
-        res.status(500).json({ message: error.message })
-    }
 })
 
 mongoose.connect(MONGO_URI)
